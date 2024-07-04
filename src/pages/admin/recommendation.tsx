a1dee02251a51b23a26f2061ac7478ec981e24f9
GadiegoN <ngadiego@gmail.com>
2024-07-04 18:24:09 -0300

    feat: add recommendation

--- src/pages/admin/recommendation.tsx
+++ src/pages/admin/recommendation.tsx
@@ -1,126 +1,153 @@
-import { TextInput } from "@/components/text-input";
 import { Button } from "@/components/ui/button";
 import { AuthContext } from "@/context/auth-context";
 import { db } from "@/services/firebase-connection";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
-import { useContext, useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { addDoc, collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
+import { Star, Trash } from "lucide-react";
+import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
 
-const schema = z.object({
-    recommendation: z.string(),
-    rating: z.string().max(1, "A nota precisa ser de 0 a 5")
-});
-
-type RecommendationFormData = z.infer<typeof schema>;
-
-interface AvatarProps {
-    name: string;
-    uid: string;
-    previewUrl: string;
-    url: string;
-}
-
-interface ProfileProps {
+interface ReviewsProps {
     id: string;
-    uid: string;
     name: string;
-    about: string;
-    avatar: AvatarProps[];
+    rating: number;
+    comment: string;
+    created: Date;
 }
 
 export function Recommendation() {
-    const { user } = useContext(AuthContext);
-    const { register, handleSubmit, formState: { errors } } = useForm<RecommendationFormData>({
-        resolver: zodResolver(schema),
-        mode: "onChange"
-    });
+    const { user } = useContext(AuthContext)
+    const [rating, setRating] = useState<number>(0);
+    const [comment, setComment] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [reviews, setReviews] = useState<ReviewsProps[]>([])
 
-    const [profile, setProfile] = useState<ProfileProps | null>(null); // Mudança para um único perfil
+    function handleRatingChange(e: ChangeEvent<HTMLInputElement>) {
+        setRating(parseInt(e.target.value, 10))
+    }
+
+    function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value)
+    }
+
+    function handleCommentChange(e: ChangeEvent<HTMLTextAreaElement>) {
+        setComment(e.target.value)
+    }
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        addDoc(collection(db, "reviews"), {
+            uid: user?.uid,
+            name,
+            email: user?.email,
+            rating,
+            comment,
+            created: new Date()
+        })
+            .then(() => {
+                setRating(0)
+                setComment('')
+                alert("Avaliação enviada com sucesso!")
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+
+    }
 
     useEffect(() => {
-        async function loadProfile() {
+        async function loadReviews() {
             if (!user?.uid) {
-                return;
+                return
             }
 
-            const profileRef = collection(db, "profile");
-            const queryRef = query(profileRef, where("uid", "==", user.uid));
+            const reviewsRef = collection(db, "reviews")
+            const queryRef = query(reviewsRef, where("uid", "==", user.uid))
 
             getDocs(queryRef)
                 .then((snapshot) => {
-                    if (!snapshot.empty) {
-                        const doc = snapshot.docs[0];
-                        setProfile({
+                    const listReviews = [] as ReviewsProps[]
+
+                    snapshot.forEach((doc) => {
+                        listReviews.push({
                             id: doc.id,
-                            uid: doc.data().uid,
                             name: doc.data().name,
-                            about: doc.data().about,
-                            avatar: doc.data().avatar
-                        });
-                    } else {
-                        setProfile(null);
-                    }
-                });
+                            rating: doc.data().rating,
+                            comment: doc.data().comment,
+                            created: doc.data().created,
+                        })
+                    })
+
+                    setReviews(listReviews)
+                })
         }
 
-        loadProfile();
-    }, [user]);
+        loadReviews()
+    }, [user])
 
-    async function onSubmit(data: RecommendationFormData) {
-        if (!user || !profile) {
-            alert("Precisa ter um perfil criado!");
-            return;
-        }
+    async function handleDeleteReview(review: ReviewsProps) {
+        const itemReview = review
 
-        try {
-            await addDoc(collection(db, "recommendations"), {
-                recommendation: data.recommendation,
-                rating: data.rating,
-                user: {
-                    uid: user.uid,
-                    email: user.email,
-                    name: profile.name,
-                    avatar: profile.avatar
-                }
-            });
+        const docRef = doc(db, "reviews", itemReview.id)
+        await deleteDoc(docRef)
 
-            alert("Recomendação enviada com sucesso!");
-        } catch (error) {
-            console.error(error);
-            alert("Erro ao enviar a recomendação!");
-        }
+        alert("Avaliação deletada com sucesso!")
     }
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <TextInput
-                    textarea
-                    register={register}
-                    name="recommendation"
-                    label="Recomendação"
-                    erro={errors.recommendation?.message}
-                    placeholder="Digite sua recomendação..."
-                />
-                <div>
-                    <label>
-                        Nota:
-                        <input
-                            type="number"
-                            {...register("rating")}
-                            placeholder="Digite sua nota..."
-                            min="0"
-                            max="5"
-                            className="w-full flex h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-                        />
-                    </label>
-                    {errors.rating && <p>{errors.rating.message}</p>}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="" className="block mb-1 font-semibold">Avalie:</label>
+                <div className="flex items-center">
+                    {[1, 2, 3, 4, 5].map((value) => (
+                        <label key={value} className="ml-1">
+                            <input
+                                type="radio"
+                                name="rating"
+                                value={value}
+                                checked={rating === value}
+                                onChange={handleRatingChange}
+                                className="sr-only"
+                            />
+                            <span className="cursor-pointer text-xl">{value <= rating ? "⭐" : <Star className="mt-1" />}</span>
+                        </label>
+                    ))}
                 </div>
-
-                <Button className="w-full mt-4" type="submit">Salvar</Button>
+                <div>
+                    <label htmlFor="name" className="block mb-1 font-semibold">Nome:</label>
+                    <input
+                        id="name"
+                        value={name}
+                        onChange={handleNameChange}
+                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                        placeholder="Deixe seu nome"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="comment" className="block mb-1 font-semibold">Comentário:</label>
+                    <textarea
+                        id="comment"
+                        value={comment}
+                        onChange={handleCommentChange}
+                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                        rows={4}
+                        placeholder="Deixe seu comentário..."
+                    />
+                </div>
+                <div>
+                    <Button type="submit">Enviar avaliação</Button>
+                </div>
             </form>
+
+            <div className="flex flex-wrap gap-4">
+                {reviews.map((item) => (
+                    <div key={item.id} className="shadow-lg border p-4 rounded-lg">
+                        <p>{item.name}</p>
+                        <p>{item.comment}</p>
+                        <p>{item.rating}</p>
+                        <Trash className="size-6 text-red-500" onClick={() => handleDeleteReview(item)} />
+                    </div>
+                ))}
+            </div>
         </div>
-    );
+    )
 }
